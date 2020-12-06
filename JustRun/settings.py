e1b4073584a0c2608a29acfc20942cb0c11489bd
Aritra Mondal <mondalaritra002@gmail.com>
2020-12-06 19:59:07 +0530

    Deploying

--- JustRun/settings.py
+++ JustRun/settings.py
@@ -78,14 +78,20 @@ WSGI_APPLICATION = 'JustRun.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'justrun',
-        'USER': 'aritra1999',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'justrun',
+#         'USER': 'aritra1999',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
